4e4cc97c0b25e32f526c8e8efb3d0f10d21155d9
Natsir <hentihu.natsir@gmail.com>
2025-09-15 13:26:00 +0700

    fix: add cloud connection for deploy

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: "http://localhost:5173", // asal frontend kamu
+    origin: process.env.FRONTEND_URL,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true, // kalau pakai cookie/session
